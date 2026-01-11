a83d8ba61dd46f79303d746b5e38af0ccdc12edf
Sergey <sbalitskiy@gmail.com>
2026-01-11 23:12:58 +0100

    Change addEventListener to return EventSubscription

--- src/react/native/api/AppState.hx
+++ src/react/native/api/AppState.hx
@@ -3,8 +3,12 @@ package react.native.api;
 @:jsRequire('react-native', 'AppState')
 extern class AppState {
 	static var currentState:AppStateValue;
-	static function addEventListener(event:AppStateEventName, listener:AppStateValue->Void):Void;
+	static function addEventListener(event:AppStateEventName, listener:AppStateValue->Void):EventSubscription;
 	static function removeEventListener(event:AppStateEventName, listener:AppStateValue->Void):Void;
+}
+
+typedef EventSubscription = {
+	function remove():Void;
 }
 
 @:enum
